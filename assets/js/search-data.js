b722c870af501a05f9038c57747594fe6f7c509e
lucl13 <cl4354@cumc.columbia.edu>
2026-04-17 17:45:11 +0000

    Deploying to gh-pages from @ lucl13/lucl13.github.io@1146a5e92779ba2c23abb63c89960fb97ef53a23 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -95,7 +95,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%6C%75%63%6C%74%73%69%6E%67%68%75%61@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+          window.open("mailto:%63%68%65%6E%6C%69%6E.%68%65%6C%69%78@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-scholar',
